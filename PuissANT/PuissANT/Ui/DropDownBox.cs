206f5b8cf36c79cfa3f28c4378f9680c0fdb1362
unknown <jbtyson@N126W72.win.cis.ksu.edu>
2014-12-08 14:20:23 -0600

    added dropdown button

--- PuissANT/PuissANT/Ui/DropDownBox.cs
+++ PuissANT/PuissANT/Ui/DropDownBox.cs
@@ -12,17 +12,17 @@ namespace PuissANT.Ui
 {
     public class DropDownBox
     {
-        public Button Top;
+        public Button Top, Dropdown;
         public List<Button> Children;
-        public Vector2 Position, Dimensions;
+        public Vector2 Position, PherDimensions, DropdownDimensions;
         public Rectangle Hitbox;
         public string Text;
         public bool Expanded;
 
         public DropDownBox()
         {
-            Position = Dimensions = Vector2.Zero;
-            Top = new Button();
+            Position = PherDimensions = DropdownDimensions = Vector2.Zero;
+            Top = Dropdown = new Button();
             Children = new List<Button>();
             Text = String.Empty;
             Hitbox = new Rectangle();
@@ -31,7 +31,8 @@ namespace PuissANT.Ui
 
         public void LoadContent()
         {
-            Dimensions = new Vector2(150, 54);
+            PherDimensions = new Vector2(150, 32);
+            DropdownDimensions = new Vector2(32, 32);
             Position = new Vector2(1000, 50);
             foreach (TileInfo t in TileInfoSets.PheromoneTypes)
             {
@@ -40,8 +41,8 @@ namespace PuissANT.Ui
                 string pressedPath = "ui/" + t.ToString().Trim().ToLower() + "PressedButton";
 
                 Button b = new Button();
-                b.Location = new Rectangle((int)Position.X, (int)Position.Y + Children.Count * (int)Dimensions.Y,
-                    (int)Dimensions.X, (int)Dimensions.Y);
+                b.Location = new Rectangle((int)Position.X, (int)Position.Y + (Children.Count + 1) * (int)PherDimensions.Y,
+                    (int)PherDimensions.X, (int)PherDimensions.Y);
                 // Create the text
                 b.Font = new Image().Font;
                 b.ButtonText = String.Empty;
@@ -49,12 +50,26 @@ namespace PuissANT.Ui
                 b.SetTexture(Game1.Instance.Content.Load<Texture2D>(neutralPath), Button.ButtonState.Neutral);
                 b.SetTexture(Game1.Instance.Content.Load<Texture2D>(overPath), Button.ButtonState.Over);
                 b.SetTexture(Game1.Instance.Content.Load<Texture2D>(pressedPath), Button.ButtonState.Pressed);
-                b.ButtonClicked = HandleButtonClick;
+                b.ButtonClicked = HandlePherButtonClick;
                 b.Value = t;
                 Children.Add(b);
             }
 
-            Top = Children[0];
+            // Add the top button image
+            Top = Children[0].Clone();
+            Top.Location = new Rectangle((int)Position.X, (int)Position.Y,
+                    (int)PherDimensions.X, (int)PherDimensions.Y);
+            //Add the dropdown button
+            Dropdown.Location = new Rectangle((int)Position.X + (int)PherDimensions.X, (int)Position.Y,
+                    (int)DropdownDimensions.X, (int)DropdownDimensions.Y);
+            // Create the text
+            Dropdown.Font = new Image().Font;
+            Dropdown.ButtonText = String.Empty;
+            // Set the image textures...this is retarded, fix later
+            Dropdown.SetTexture(Game1.Instance.Content.Load<Texture2D>("ui/dropdownDOWN"), Button.ButtonState.Neutral);
+            Dropdown.SetTexture(Game1.Instance.Content.Load<Texture2D>("ui/dropdownDOWN"), Button.ButtonState.Over);
+            Dropdown.SetTexture(Game1.Instance.Content.Load<Texture2D>("ui/dropdownDOWN"), Button.ButtonState.Pressed);
+            Dropdown.ButtonClicked = HandleDropDownButtonClick;
         }
 
         public void UnloadContent()
@@ -65,21 +80,37 @@ namespace PuissANT.Ui
         public void Update(GameTime gameTime)
         {
             Top.Update(gameTime);
+            Dropdown.Update(gameTime);
             foreach (Button b in Children)
                 b.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            Dropdown.Draw(spriteBatch);
             Top.Draw(spriteBatch);
             if(Expanded)
                 foreach (Button b in Children)
                     b.Draw(spriteBatch);
         }
 
-        public void HandleButtonClick(object sender)
+        public void HandlePherButtonClick(Button sender)
         {
-            PheromoneManger.Instance.HandlePheremoneButtonClick((TileInfo)sender);
+            if (!Expanded)
+                PheromoneManger.Instance.HandlePheremoneButtonClick((TileInfo)sender.Value);
+            else
+            {
+                Top = (Button)sender.Clone();
+                Top.Location = new Rectangle((int)Position.X, (int)Position.Y,
+                    (int)PherDimensions.X, (int)PherDimensions.Y);
+                Expanded = false;
+                PheromoneManger.Instance.HandlePheremoneButtonClick((TileInfo)sender.Value);
+            }
+        }
+
+        public void HandleDropDownButtonClick(Button sender)
+        {
+            Expanded = !Expanded;
         }
     }
 }
