ba34acbbd2ec1e6ee37c4bfd542653e6c93c170a
jbtyson <jbtyson@ksu.edu>
2014-12-08 00:22:41 -0600

    added missing resources

---
+++ PuissANT/PuissANT/Ui/DropDownBox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PuissANT.Ui
+{
+    public class DropDownBox
+    {
+        public Image Image;
+        public List<Image> Children;
+        public Vector2 Position, Dimensions;
+        public string Text;
+
+        public DropDownBox()
+        {
+            Position = Dimensions = Vector2.Zero;
+            Image = new Image();
+            Children = new List<Image>();
+            Text = String.Empty;
+        }
+
+        public void LoadContent()
+        {
+            
+        }
+
+        public void UnloadContent()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
